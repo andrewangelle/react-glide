c042846ae3a36b2435eb3ef6536908c283fdb077
Andrew Angelle <andrewangelle@gmail.com>
2022-10-23 13:19:28 -0400

    refactor: use vite, vitest, and eslint

--- src/tests/setupTests.ts
+++
@@ -1,5 +0,0 @@
-/* A light wrapper to apply enzyme adapter configuration and expose enzyme members */
-
-import 'raf/polyfill';
-import 'regenerator-runtime/runtime'
-import '@testing-library/jest-dom'
