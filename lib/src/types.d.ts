12aec7c46ba96a1d775072605db99a37842d7adb
Andrew Angelle <andrewangelle@gmail.com>
2022-10-23 13:35:33 -0400

    5.0.0-alpha.0

---
+++ lib/src/types.d.ts
@@ -0,0 +1,21 @@
+export interface PreloaderProps {
+    currentIndex: number;
+    width: number;
+}
+export interface PreloaderState {
+    loading: boolean;
+    done: boolean;
+    loadCount: number;
+}
+export interface GlideState {
+    currentIndex: number;
+}
+export interface GlideProps {
+    autoPlay?: boolean;
+    autoPlaySpeed?: number;
+    dots?: boolean;
+    height?: number;
+    infinite?: boolean;
+    width: number | string;
+    onSlideChange?: () => void;
+}
