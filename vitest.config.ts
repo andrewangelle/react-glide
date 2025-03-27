fab0cac51983e00efed53a2bbf4adc1d85e52b01
Andrew Angelle <andrewangelle@gmail.com>
2025-03-27 10:37:54 -0400

    test: remove reference types

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,3 @@
-/// <reference types='vitest' />
-/// <reference types='vite/client' />
-
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
