40a34a293bbe25a9b35ed84d9996fa14758227f0
Andrew Angelle <andrewangelle@gmail.com>
2025-03-27 18:29:45 -0400

    chore: Update example

--- examples/tanstackstart-react-18/app/routes/__root.tsx
+++ examples/tanstackstart-react-18/app/routes/__root.tsx
@@ -19,7 +19,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Andrew Angelle',
+        title: 'React Glide Example',
       },
       {
         rel: 'icon',
