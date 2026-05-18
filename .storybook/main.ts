0cda1956c1efdc3398b75b4caf5a9f0717a78dc2
Andrew Angelle <andrewangelle@gmail.com>
2026-05-18 17:34:24 -0400

    test: fix storybook build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,7 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import react from "@vitejs/plugin-react";
+import { mergeConfig } from "vite";
+
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 
@@ -19,5 +23,14 @@ export default {
 
   typescript: {
     reactDocgen: "react-docgen-typescript",
+  },
+
+  async viteFinal(config: StorybookConfig) {
+    return mergeConfig(config, {
+      resolve: {
+        tsconfigPaths: true,
+      },
+      plugins: [react()],
+    });
   },
 };
